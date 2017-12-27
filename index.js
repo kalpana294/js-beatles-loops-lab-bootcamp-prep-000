cf291af39e85c29720be699a6e0bcf5e6b4e9189
Kalpana Swaminathan <kalpana294@gmail.com>
2017-12-27 21:54:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function theBeatlesPlay(musicians,instruments){
+  var music = []
+  for(var i=0;i<4;i++){
+    music.push(musicians[i] + " plays "+instruments[i]);
+    }
+    return music
+}
+
+function johnLennonFacts(facts){
+  while(i>=0 ){
+    facts[i].
+  }
+  
+}
